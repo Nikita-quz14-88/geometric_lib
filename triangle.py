48e1ffc58f7e4f23dc7640fd97e501f4be4c3f6f
Aslamov Nikita <nikitaaslamov977@gmail.com>
2024-12-18 18:26:15 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,9 +1,11 @@
 from math import sqrt
 def area(a, b, c):
-    if a < 0 or b < 0 or c < 0:
-        raise AssertionError("Стороны не могут быть отрицательными")
-    x = (a + b + c) / 2
-    return sqrt(x * (x - a) * (x - b) * (x - c))
+    if a <= 0 or b <= 0 or c <= 0:
+        raise AssertionError("Стороны должны быть положительными")
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise AssertionError("Треугольник с такими сторонами не существует")
+    s = (a + b + c) / 2
+    return sqrt(s * (s - a) * (s - b) * (s - c))
 def perimeter(a, b, c):
     if a < 0 or b < 0 or c < 0:
         raise AssertionError("Стороны не могут быть отрицательными")
